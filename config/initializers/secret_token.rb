d49bffafdf211401559c52bbeeb286aa76e7fe38
student knowledge of HTML, CSS, and basics php.(студент, знание HTML, CSS, и основы php) <vitiaivliev@gmail.com>
2014-04-29 17:55:59 +0300

    start
    clean slash template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialNetwork::Application.config.secret_key_base = '54a6070e28d08340d15f01b1c95dec0eb0cc9cd9774da99284d4afedace67c5d05f820dab598ef0dc2ccd5ab7c53459f1c20d526f7849a9c60a0753213c03b69'
